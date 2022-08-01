cd2a9d9a79c84c7ce04275c799ded4a67a090b3c
Owenvii <owen9456@gmail.com>
2022-08-01 16:04:05 -0600

    Improvements to the Teleportation / Room Joining System
    * Moved methods for teleportation into the player object
    * Added functionality for teleporting to a player in a sanctuary
    * Standarized and centralized all code for teleportation logic
    * Still Needs Testing..

--- src/main/java/org/asf/emuferal/packets/xt/gameserver/social/JumpToPlayer.java
+++ src/main/java/org/asf/emuferal/packets/xt/gameserver/social/JumpToPlayer.java
@@ -4,15 +4,9 @@ import java.io.IOException;
 
 import org.asf.emuferal.data.XtReader;
 import org.asf.emuferal.data.XtWriter;
-import org.asf.emuferal.entities.generic.Quaternion;
-import org.asf.emuferal.entities.generic.Vector3;
-import org.asf.emuferal.entities.uservars.UserVarValue;
-import org.asf.emuferal.networking.gameserver.GameServer;
 import org.asf.emuferal.networking.smartfox.SmartfoxClient;
 import org.asf.emuferal.packets.xt.IXtPacket;
-import org.asf.emuferal.packets.xt.gameserver.world.JoinRoom;
 import org.asf.emuferal.players.Player;
-import org.asf.emuferal.social.SocialManager;
 
 public class JumpToPlayer implements IXtPacket<JumpToPlayer> {
 
@@ -42,71 +36,7 @@ public class JumpToPlayer implements IXtPacket<JumpToPlayer> {
 	@Override
 	public boolean handle(SmartfoxClient client) throws IOException {
 		Player player = ((Player) client.container);
-
-		// Find player
-		for (Player plr : ((GameServer) client.getServer()).getPlayers()) {
-			if (plr.account.getAccountID().equals(accountID) && plr.roomReady && !plr.room.equals("room_STAFFROOM")
-					&& (!SocialManager.getInstance().socialListExists(accountID) || !SocialManager.getInstance()
-							.getPlayerIsBlocked(accountID, player.account.getAccountID()))) {
-				// Load privacy settings
-				int privSetting = 0;
-				UserVarValue val = plr.account.getPlayerInventory().getUserVarAccesor().getPlayerVarValue(17546, 0);
-				if (val != null)
-					privSetting = val.value;
-
-				// Verify privacy settings
-				if (privSetting == 1 && !SocialManager.getInstance().getPlayerIsFollowing(plr.account.getAccountID(),
-						player.account.getAccountID()))
-					break;
-				else if (privSetting == 2)
-					break;
-
-				XtWriter writer = new XtWriter();
-				writer.writeString("rfjtr");
-				writer.writeInt(-1); // data prefix
-				writer.writeInt(1); // other world
-				writer.writeString("");
-				writer.writeString(""); // data suffix
-				client.sendPacket(writer.encode());
-
-				if (!plr.room.equals(player.room)) {
-					// Build room join
-					JoinRoom join = new JoinRoom();
-					join.levelType = plr.levelType;
-					join.levelID = plr.levelID;
-					join.roomIdentifier = "room_" + join.levelID;
-					player.teleportDestination = plr.account.getAccountID();
-					player.targetPos = new Vector3(plr.lastPosX, plr.lastPosY, plr.lastPosZ);
-					player.targetRot = new Quaternion(plr.lastRotX, plr.lastRotY, plr.lastRotZ, plr.lastRotW);
-
-					// Sync
-					GameServer srv = (GameServer) client.getServer();
-					for (Player plr2 : srv.getPlayers()) {
-						if (plr2.room != null && player.room != null && player.room != null
-								&& plr2.room.equals(player.room) && plr2 != player) {
-							player.destroyAt(plr2);
-						}
-					}
-
-					// Assign room
-					player.roomReady = false;
-					player.pendingLevelID = plr.levelID;
-					player.pendingRoom = plr.room;
-					player.levelType = plr.levelType;
-
-					// Send packet
-					client.sendPacket(join);
-				}
-				return true;
-			}
-		}
-
-		XtWriter writer = new XtWriter();
-		writer.writeString("rfjtr");
-		writer.writeInt(-1); // data prefix
-		writer.writeInt(0); // failure
-		writer.writeString(""); // data suffix
-		client.sendPacket(writer.encode());
+		player.teleportToPlayer(accountID);
 		return true; // Account not found, blocked or still loading
 	}
 
